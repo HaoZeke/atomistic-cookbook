404c65a968c669135f1869ec7c5c04614319d203
ppegolo <paolo.pegolo@epfl.ch>
2024-12-10 11:09:28 +0100

    First commit

---
+++ examples/cg-product/README.rst
@@ -0,0 +1,5 @@
+Computing Clebsch-Gordan products with Featomic
+===============================================
+
+This example illustrates how one can compute Clebsch-Gordan (CG) 
+products of two ``metatensor`` ``TensorMap`` with ``featomic``.
---
+++ examples/cg-product/cg-product.py
@@ -0,0 +1,25 @@
+"""
+Clebsh-Gordan Products Tutorial
+===============================
+
+:Authors: Joe W. Abbott `@jwa7 <https://github.com/jwa7>`__,
+          Paolo Pegolo `@ppegolo <https://github.com/ppegolo>`__
+
+This tutorial explains how to ...
+"""
+
+# %%
+# First, import the necessary packages
+#
+
+# %%
+# 1. Compute a Spherical Harmonic Expansion
+
+# %%
+# 2. Compute the Clebsch-Gordan Coefficients
+
+# %%
+# 3. Compute an Equivariant Power Spectrum
+
+# %%
+# 4. Compute an Equivariant Power Spectrum By Pair
